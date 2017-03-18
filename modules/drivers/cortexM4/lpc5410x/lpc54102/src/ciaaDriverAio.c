1799ea5defd780a91ed41c4dce2d7a1019f09555
Gerardo Puga <glpuga@gmail.com>
2017-03-18 17:26:01 -0300

    Renamed the ADC driver to AIO, which is the same name used in other ports.

--- modules/drivers/cortexM4/lpc5410x/lpc54102/src/ciaaDriverAio.c
+++ modules/drivers/cortexM4/lpc5410x/lpc54102/src/ciaaDriverAio.c
@@ -1,5 +1,4 @@
-/* Copyright 2014, Fernando Beunza
- * Copyright 2017, Gerardo Puga
+/* Copyright 2017, Gerardo Puga
  *
  * All rights reserved.
  *
@@ -33,9 +32,9 @@
  *
  */
 
-/** \brief CIAA Aio Driver for LPC4337
+/** \brief CIAA UART Driver for LPC54102
  **
- ** Implements the A/D & D/A Drivers for LPC4337
+ ** Implements the UART Driver for LPC54102
  **
  **/
 
@@ -43,7 +42,7 @@
  ** @{ */
 /** \addtogroup Drivers CIAA Drivers
  ** @{ */
-/** \addtogroup AIO AIO Drivers
+/** \addtogroup ADC Drivers
  ** @{ */
 
 /*==================[inclusions]=============================================*/
@@ -51,10 +50,30 @@
 
 
 #include "ciaaDriverAio.h"
+#include "ciaaDriverCommon.h"
+#include "chip.h"
 
 
 
 /*==================[macros and definitions]=================================*/
+
+
+
+#define CIAA_DRIVER_AIO_LPC54102_ADC_PORTS         (sizeof(lpc54102AioConfigurationStructures) / sizeof(lpc54102AioConfigurationStructuresType))
+
+#define CIAA_DRIVER_AIO_LPC54102_ADC_FREQUENCY     10000000
+
+
+typedef struct {
+
+   int32_t aioChannel;
+
+   char const * posixName;       /* Name of the device on the POSIX device tree. */
+
+   uint32_t lpcIoconPort;
+   uint32_t lpcIoconPin;
+
+} lpc54102AioConfigurationStructuresType;
 
 
 
@@ -70,6 +89,51 @@
 
 
 
+const lpc54102AioConfigurationStructuresType lpc54102AioConfigurationStructures[] =
+      {
+            {
+                  9,                                  /* aioChannel         */
+                  "aio/0",                            /* posixName          */
+                  1,                                  /* lpcIoconPort       */
+                  6                                   /* lpcIoconPin        */
+            },
+            {
+                  7,                                  /* aioChannel         */
+                  "aio/1",                            /* posixName          */
+                  1,                                  /* lpcIoconPort       */
+                  4                                   /* lpcIoconPin        */
+            },
+            {
+                  6,                                  /* aioChannel         */
+                  "aio/2",                            /* posixName          */
+                  1,                                  /* lpcIoconPort       */
+                  3                                   /* lpcIoconPin        */
+            },
+            {
+                  5,                                  /* aioChannel         */
+                  "aio/3",                            /* posixName          */
+                  1,                                  /* lpcIoconPort       */
+                  2                                   /* lpcIoconPin        */
+            },
+            {
+                  4,                                  /* aioChannel         */
+                  "aio/4",                            /* posixName          */
+                  1,                                  /* lpcIoconPort       */
+                  1                                   /* lpcIoconPin        */
+            },
+            {
+                  3,                                  /* aioChannel         */
+                  "aio/5",                            /* posixName          */
+                  1,                                  /* lpcIoconPort       */
+                  0                                   /* lpcIoconPin        */
+            }
+      };
+
+
+ciaaDevices_deviceType lpc54102AioPosixRegistrationDataTable[CIAA_DRIVER_AIO_LPC54102_ADC_PORTS];
+
+
+
 /*==================[external data definition]===============================*/
 
 
@@ -77,8 +141,90 @@
 /*==================[internal functions definition]==========================*/
 
 
+void ciaaDriverAioLpc54102_SetAioInputPinInSafeMode(uint32_t port, uint32_t pin)
+{
+   uint32_t mode;
 
-/*==================[internal functions definition]==========================*/
+   mode = ciaaDriverCommonLpc54102_determineInputPinMode(port, pin);
+
+   Chip_IOCON_PinMux(LPC_IOCON, port, pin, mode, IOCON_FUNC0);
+
+   Chip_GPIO_SetDir(LPC_GPIO, port, pin, 0); /* INPUT */
+}
+
+
+void ciaaDriverAioLpc54102_SetAioInputPinInActiveMode(uint32_t port, uint32_t pin)
+{
+   Chip_IOCON_PinMux(LPC_IOCON, port, pin, IOCON_ANALOG_EN | IOCON_INPFILT_OFF, IOCON_FUNC0);
+
+   Chip_GPIO_SetDir(LPC_GPIO, port, pin, 0); /* INPUT */
+}
+
+
+void ciaaDriverAioLpc54102_InitializeControlStructures()
+{
+   int32_t usartIndex;
+   int32_t devIndex;
+
+   for (devIndex = 0; devIndex < CIAA_DRIVER_AIO_LPC54102_ADC_PORTS; devIndex++)
+   {
+      lpc54102AioPosixRegistrationDataTable[devIndex].path    = lpc54102AioConfigurationStructures[devIndex].posixName;
+
+      lpc54102AioPosixRegistrationDataTable[devIndex].open    = ciaaDriverAio_open;
+      lpc54102AioPosixRegistrationDataTable[devIndex].close   = ciaaDriverAio_close;
+      lpc54102AioPosixRegistrationDataTable[devIndex].read    = ciaaDriverAio_read;
+      lpc54102AioPosixRegistrationDataTable[devIndex].write   = ciaaDriverAio_write;
+      lpc54102AioPosixRegistrationDataTable[devIndex].ioctl   = ciaaDriverAio_ioctl;
+      lpc54102AioPosixRegistrationDataTable[devIndex].lseek   = NULL;
+
+      lpc54102AioPosixRegistrationDataTable[devIndex].upLayer = NULL;
+      lpc54102AioPosixRegistrationDataTable[devIndex].layer   = (void *)&lpc54102AioConfigurationStructures[devIndex];
+      lpc54102AioPosixRegistrationDataTable[devIndex].loLayer = NULL;
+   }
+}
+
+
+void ciaaDriverAioLpc54102_hardwareInit()
+{
+   int32_t devIndex;
+
+   Chip_ADC_Init(
+         LPC_ADC,
+         ADC_CR_RESOL(0x01) |             /* 8 bits per sample */
+         ADC_CR_TSAMP(ADC_TSAMP_5CLK5));  /* Minimum required sample times for slow channels. */
+
+   Chip_ADC_Calibration(
+         LPC_ADC);
+
+   Chip_ADC_SetClockRate(
+         LPC_ADC,
+         CIAA_DRIVER_AIO_LPC54102_ADC_FREQUENCY);
+
+   /*
+    * Set the corresponding input pins in a safe mode, digital with
+    * input over-voltage protection hardware.
+    * Pins will only be turned into analog  mode when the driver device
+    * file is opened.
+    */
+
+   for (devIndex = 0; devIndex < CIAA_DRIVER_AIO_LPC54102_ADC_PORTS; devIndex++)
+   {
+      ciaaDriverAioLpc54102_SetAioInputPinInSafeMode(
+            lpc54102AioConfigurationStructures[devIndex].lpcIoconPort,
+            lpc54102AioConfigurationStructures[devIndex].lpcIoconPin);
+   }
+}
+
+
+void ciaaDriverAioLpc54102_registerDevices()
+{
+   int32_t devIndex;
+
+   for (devIndex = 0; devIndex < CIAA_DRIVER_AIO_LPC54102_ADC_PORTS; devIndex++)
+   {
+      ciaaDioDevices_addDriver(&lpc54102AioPosixRegistrationDataTable[devIndex]);
+   }
+}
 
 
 
@@ -88,48 +234,104 @@
 
 extern ciaaDevices_deviceType * ciaaDriverAio_open(char const * path, ciaaDevices_deviceType * device, uint8_t const oflag)
 {
+   lpc54102AioConfigurationStructuresType *dev;
+
+   dev = (lpc54102AioConfigurationStructuresType *)device->layer;
+
+   /* Set the input pin into analog mode */
+   ciaaDriverAioLpc54102_SetAioInputPinInActiveMode(dev->lpcIoconPort, dev->lpcIoconPin);
+
    return device;
 }
 
 
 extern int32_t ciaaDriverAio_close(ciaaDevices_deviceType const * const device)
 {
-   return -1;
+   lpc54102AioConfigurationStructuresType *dev;
+
+   dev = (lpc54102AioConfigurationStructuresType *)device->layer;
+
+   /* Put the input pin back in a safe digital over-voltage protected mode. */
+   ciaaDriverAioLpc54102_SetAioInputPinInSafeMode(dev->lpcIoconPort, dev->lpcIoconPin);
+
+   return 0;
 }
 
 
 extern int32_t ciaaDriverAio_ioctl(ciaaDevices_deviceType const * const device, int32_t const request, void * param)
 {
-   int32_t ret = -1;
-
-   return ret;
+   return -1;
 }
 
 
 extern ssize_t ciaaDriverAio_read(ciaaDevices_deviceType const * const device, uint8_t* buffer, size_t const size)
 {
-   ssize_t ret = -1;
+   lpc54102AioConfigurationStructuresType *dev;
+   uint32_t dataRegister;
 
-   return ret;
+   dev = (lpc54102AioConfigurationStructuresType *)device->layer;
+
+   for (i = 0; i < size; i++)
+   {
+
+      Chip_ADC_DisableSequencer(
+            LPC_ADC,
+            ADC_SEQA_IDX);
+
+      Chip_ADC_SetupSequencer(
+            LPC_ADC,
+            ADC_SEQA_IDX,
+            ADC_SEQ_CTRL_CHANSEL(dev->aioChannel));
+
+      Chip_ADC_EnableSequencer(
+            LPC_ADC,
+            ADC_SEQA_IDX);
+
+      Chip_ADC_StartSequencer(
+            LPC_ADC,
+            ADC_SEQA_IDX);
+
+      /*
+       * This loop should only wait for about 15 ADC conversion
+       * clock cycles for each sample.
+       *  */
+
+      do {
+
+         dataRegister = Chip_ADC_GetDataReg(LPC_ADC, dev->aioChannel);
+
+      } while ((dataRegister & ADC_DR_DATAVALID) == 0);
+
+      buffer[i] = (dataRegister >> 8) & 0x00ff;
+
+   }
+
+   Chip_ADC_DisableSequencer(
+         LPC_ADC,
+         ADC_SEQA_IDX);
+
+   return size;
 }
 
 
 extern ssize_t ciaaDriverAio_write(ciaaDevices_deviceType const * const device, uint8_t const * const buffer, size_t const size)
 {
-   ssize_t ret = -1;
-
-   return ret;
+   return -1;
 }
 
 
 void ciaaDriverAio_init(void)
 {
+   ciaaDriverAioLpc54102_InitializeControlStructures();
 
+   ciaaDriverAioLpc54102_hardwareInit();
+
+   ciaaDriverAioLpc54102_registerDevices()
 }
 
 
-/*==================[interrupt handlers]=====================================*/
 
+/*==================[interrupt handlers]=====================================*/
 
 
 
